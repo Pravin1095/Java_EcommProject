d3124f433239911d0355c945e8ededbf7d10ca3f
Pravin A <a.pravin3210@gmail.com>
2026-02-02 20:34:50 +0530

    Creating OrderControllers

---
+++ Backend/src/main/java/com/pravin/EcommProject/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.pravin.EcommProject.service;
+
+import com.pravin.EcommProject.model.dto.OrderRequest;
+import com.pravin.EcommProject.model.dto.OrderResponse;
+
+import java.util.List;
+
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
